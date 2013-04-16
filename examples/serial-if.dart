76f6ce0f4a4747b95558e823dec736238588bfb9
Jacob Nelson <nelson@cs.washington.edu>
2013-04-16 16:47:10 -0700

    simplify serial if example

--- examples/serial-if.dart
+++ examples/serial-if.dart
@@ -1,210 +1,111 @@
 
 import 'stdio.dart';
 
-int n1;
-int n2;
-int n3;
-int n4;
-int n5;
-int n6;
-int n7;
-int n8;
-int n9;
-int n10;
-int n11;
-int n12;
-int n13;
-int n14;
-int n15;
-int n16;
-int n17;
-int n18;
-int n19;
-int n20;
-int n21;
-int n22;
-int n23;
-int n24;
-int n25;
-int n26;
-int n27;
-int n28;
-int n29;
-int n30;
-int n31;
-int n32;
-int n33;
-int n34;
-int n35;
-int n36;
-int n37;
-int n38;
-int n39;
-int n40;
-int n41;
-int n42;
-int n43;
-int n44;
-int n45;
-int n46;
-int n47;
-int n48;
-int n49;
-int n50;
-int n51;
-int n52;
-int n53;
-int n54;
-int n55;
-int n56;
-int n57;
-int n58;
-int n59;
-int n60;
-int n61;
-int n62;
-int n63;
-int n64;
-int n65;
-int n66;
-int n67;
-int n68;
-int n69;
-int n70;
-int n71;
-int n72;
-int n73;
-int n74;
-int n75;
-int n76;
-int n77;
-int n78;
-int n79;
-int n80;
-int n81;
-int n82;
-int n83;
-int n84;
-int n85;
-int n86;
-int n87;
-int n88;
-int n89;
-int n90;
-int n91;
-int n92;
-int n93;
-int n94;
-int n95;
-int n96;
-int n97;
-int n98;
-int n99;
-int n100;
+int n;
 
 int result;
 
 void main() {
 result = 0;
-if( n1 > 0 ) { }
-if( n2 > 0 ) { }
-if( n3 > 0 ) { }
-if( n4 > 0 ) { }
-if( n5 > 0 ) { }
-if( n6 > 0 ) { }
-if( n7 > 0 ) { }
-if( n8 > 0 ) { }
-if( n9 > 0 ) { }
-if( n10 > 0 ) { }
-if( n11 > 0 ) { }
-if( n12 > 0 ) { }
-if( n13 > 0 ) { }
-if( n14 > 0 ) { }
-if( n15 > 0 ) { }
-if( n16 > 0 ) { }
-if( n17 > 0 ) { }
-if( n18 > 0 ) { }
-if( n19 > 0 ) { }
-if( n20 > 0 ) { }
-if( n21 > 0 ) { }
-if( n22 > 0 ) { }
-if( n23 > 0 ) { }
-if( n24 > 0 ) { }
-if( n25 > 0 ) { }
-if( n26 > 0 ) { }
-if( n27 > 0 ) { }
-if( n28 > 0 ) { }
-if( n29 > 0 ) { }
-if( n30 > 0 ) { }
-if( n31 > 0 ) { }
-if( n32 > 0 ) { }
-if( n33 > 0 ) { }
-if( n34 > 0 ) { }
-if( n35 > 0 ) { }
-if( n36 > 0 ) { }
-if( n37 > 0 ) { }
-if( n38 > 0 ) { }
-if( n39 > 0 ) { }
-if( n40 > 0 ) { }
-if( n41 > 0 ) { }
-if( n42 > 0 ) { }
-if( n43 > 0 ) { }
-if( n44 > 0 ) { }
-if( n45 > 0 ) { }
-if( n46 > 0 ) { }
-if( n47 > 0 ) { }
-if( n48 > 0 ) { }
-if( n49 > 0 ) { }
-if( n50 > 0 ) { }
-if( n51 > 0 ) { }
-if( n52 > 0 ) { }
-if( n53 > 0 ) { }
-if( n54 > 0 ) { }
-if( n55 > 0 ) { }
-if( n56 > 0 ) { }
-if( n57 > 0 ) { }
-if( n58 > 0 ) { }
-if( n59 > 0 ) { }
-if( n60 > 0 ) { }
-if( n61 > 0 ) { }
-if( n62 > 0 ) { }
-if( n63 > 0 ) { }
-if( n64 > 0 ) { }
-if( n65 > 0 ) { }
-if( n66 > 0 ) { }
-if( n67 > 0 ) { }
-if( n68 > 0 ) { }
-if( n69 > 0 ) { }
-if( n70 > 0 ) { }
-if( n71 > 0 ) { }
-if( n72 > 0 ) { }
-if( n73 > 0 ) { }
-if( n74 > 0 ) { }
-if( n75 > 0 ) { }
-if( n76 > 0 ) { }
-if( n77 > 0 ) { }
-if( n78 > 0 ) { }
-if( n79 > 0 ) { }
-if( n80 > 0 ) { }
-if( n81 > 0 ) { }
-if( n82 > 0 ) { }
-if( n83 > 0 ) { }
-if( n84 > 0 ) { }
-if( n85 > 0 ) { }
-if( n86 > 0 ) { }
-if( n87 > 0 ) { }
-if( n88 > 0 ) { }
-if( n89 > 0 ) { }
-if( n90 > 0 ) { }
-if( n91 > 0 ) { }
-if( n92 > 0 ) { }
-if( n93 > 0 ) { }
-if( n94 > 0 ) { }
-if( n95 > 0 ) { }
-if( n96 > 0 ) { }
-if( n97 > 0 ) { }
-if( n98 > 0 ) { }
-if( n99 > 0 ) { }
-if( n100 > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
+if( n > 0 ) { }
 result = result + 1;
 }
