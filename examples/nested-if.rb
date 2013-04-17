ea4676d6f29a6dcfb2d594e6b18de9e70409d16b
Jacob Nelson <nelson@cs.washington.edu>
2013-04-17 00:57:51 -0700

    nested if file generator

---
+++ examples/nested-if.rb
@@ -0,0 +1,56 @@
+
+# generate nested-if.dart with the appropriate amount of nesting
+
+nodecount = ARGV[0].to_i
+inedgecount = ARGV[1].to_i
+
+if nodecount == 0
+  nodecount = 2000
+end
+
+if inedgecount == 0
+  inedgecount = 1
+end
+
+# count down
+total = nodecount
+
+
+
+
+puts """
+
+import 'stdio.dart';
+
+int n;
+int result;
+
+void main() {
+result = 0;
+"""
+
+while total > 0
+
+  if inedgecount > total
+    inedgecount = total
+  end
+
+  # open nested if's
+  puts "if( n > 0 ) {\n" * inedgecount
+
+  # do something!
+  puts "result = result + 1;\n"
+  
+  # close nested if's
+  puts "}\n" * inedgecount
+
+  total -= inedgecount
+
+end
+  
+# end main()
+puts """
+}
+"""
+
+
