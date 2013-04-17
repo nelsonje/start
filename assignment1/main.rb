b666b7664cb85b2dda533eb14e3c7c24f1b0fe3e
Jacob Nelson <nelson@cs.washington.edu>
2013-04-16 17:21:37 -0700

    time dominator construction

--- assignment1/main.rb
+++ assignment1/main.rb
@@ -5,7 +5,12 @@ p.read_program(ARGV[0])
 #p.build_cfgs
 p.build_bbs
 p.build_cfgs
+
+start_time = Time.now
 p.build_doms
+elapsed_time = Time.now - start_time
+puts "Dominator construction took " + elapsed_time.to_s + " seconds"
+
 p.dump_cfgs
 
 
