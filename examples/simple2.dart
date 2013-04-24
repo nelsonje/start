2624c4b384fff501504730592c59062bb1e3cb5c
Jacob Nelson <nelson@cs.washington.edu>
2013-04-23 18:34:27 -0700

    add simple phi-node example; add .gitignore to clean up git status

---
+++ examples/simple2.dart
@@ -0,0 +1,19 @@
+
+//test multiple function calls
+import 'stdio.dart';
+
+void main()
+{
+  int x;
+
+  if( 1 == 2 ) {
+    x = 1;
+  } else {
+    if( 2 == 3 ) {
+      x = 2;
+    } else {
+      x = 3;
+    }
+  }
+
+}
